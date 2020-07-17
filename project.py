4823f5ce0a50f37b6c28ba04e80ddd2ce4b30eb3
zyhhhy <zyhhhy001@163.com>
2020-07-17 22:04:21 +0800

    717latest_concise

--- project.py
+++ project.py
@@ -89,13 +89,19 @@ cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_F
 cfg.DATASETS.TRAIN = ("coco_train_2",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2
-#cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
-cfg.MODEL.WEIGHTS = "./Coco/configs/COCO-Detection/faster_rcnn_R_50_FPN_1x/137257794/model_final_b275ba.pkl"
+
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
+
 cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+cfg.SOLVER.BASE_LR = 0.0025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)#128
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # how many classes
+cfg.MODEL.RPN.NMS_THRESH = 0.75
+cfg.MODEL.ANCHOR_GENERATOR.ASPECT_RATIOS = [[0.5, 1.0, 2.0]]
+cfg.MODEL.ANCHOR_GENERATOR.SIZES = [[32, 64, 96, 108, 128]]
+
+
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 print(cfg.OUTPUT_DIR)
